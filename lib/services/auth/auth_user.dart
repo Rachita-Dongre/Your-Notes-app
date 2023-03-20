3c8cd04ed0ee8d21fb0596c5c797c3a1cee8bf24
Rachita_Dongre <rachita7dongre6.17@gmail.com>
2023-03-20 09:56:23 +0530

    added tests

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 @immutable //this class and their subclasses will never be changed upon initializaton
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified); //constructor
+  const AuthUser({required this.isEmailVerified}); //constructor
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
   //In above code, we made a copy of the firebase user and pasted it into our own user (auth user) to use that, and so that ui code isn't directly accessing the firebase and an abstraction layer is maintained
 }
