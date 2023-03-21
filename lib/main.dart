194f36208d252815855eecf7e717e8d208534b67
Rachita_Dongre <rachita7dongre6.17@gmail.com>
2023-03-21 19:08:11 +0530

    implemented functionality to update note

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yournotes/constants/routes.dart';
 import 'package:yournotes/services/auth/auth_service.dart';
 import 'package:yournotes/views/login_view.dart';
-import 'package:yournotes/views/notes/new_note_view.dart';
+import 'package:yournotes/views/notes/create_update_note_view.dart';
 import 'package:yournotes/views/notes/notes_view.dart';
 import 'package:yournotes/views/register_view.dart';
 import 'package:yournotes/views/verify_email.dart';
@@ -24,7 +24,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
